e6290ebd372d69006d90a50d3c9f40542ff116ca
arjun-rumsan <arjun.rumsan@gmail.com>
2023-11-30 10:22:38 +0545

    created a basic app with a static sample data

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,32 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingService } from '../housing.service';
+import { HousingLocation } from '../housinglocation';
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, HousingLocationComponent],
+  template: `
+    <section>
+      <form>
+        <input type="text" placeholder="Filter by city" />
+        <button class="primary" type="button">Search</button>
+      </form>
+      <section class="results">
+        <app-housing-location
+          *ngFor="let housingLocation of housingLocationList"
+          [housingLocation]="housingLocation"
+        ></app-housing-location>
+      </section>
+    </section>
+  `,
+  styleUrl: './home.component.scss',
+})
+export class HomeComponent {
+  housingLocationList: HousingLocation[] = [];
+  housingService: HousingService = inject(HousingService);
+  constructor() {
+    this.housingLocationList = this.housingService.getAllHousingLocations();
+  }
+}
